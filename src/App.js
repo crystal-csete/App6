7cbf41b7ce3d854beb98ac265da588689e4c9b06
Crystal Csete <jeannecc13@gmail.com>
2021-12-11 16:06:48 -0700

    Delete obsolete files. Add cdn for font awesome to index html file.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+/** @format */
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='appContainer'>
+      <header className='appHeader'>
+        <h1>
+          Hello there, world <i class='fas fa-clock fa-lg'></i>{" "}
+        </h1>
       </header>
     </div>
   );
